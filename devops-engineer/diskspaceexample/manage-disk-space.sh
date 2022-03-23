c2fe90867180809ddf2bb6a9c4d63d04852c33dd
Deepak Kumar J <dk2053@gmail.com>
2022-03-23 22:46:21 +0530

    Update manage-disk-space.sh

--- devops-engineer/diskspaceexample/manage-disk-space.sh
+++ devops-engineer/diskspaceexample/manage-disk-space.sh
@@ -8,7 +8,7 @@ filename=audio_.wav_$timestamp.log
 log=$path$filename
 days=2
 
-for file in $(find $path -maxdepth 1 -type f -mmin +$((60*24*$days)) -print); > $log      #find
+for file in $(find $path -maxdepth 1 -type f -mmin +$((60*24*$days)) -print); > $log 
     do      
         rm -f $file     
     done
